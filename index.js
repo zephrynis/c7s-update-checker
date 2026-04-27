cb5c53a4b7efd0510c8d0eb4de6916371116165b
Zephrynis <hello@zephrynis.me>
2026-04-27 11:55:22 +0100

    Add dotenv

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/updatecheck", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Update checker running on port ${port}`);
 });
