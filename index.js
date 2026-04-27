2060537ae650e1be1f947801e0f67ab63fe8ee95
Zephrynis <hello@zephrynis.me>
2026-04-27 15:44:45 +0100

    Basic SXC version viewing

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/updatecheck", (req, res) => {
-  res.send("Hello World!");
+app.get("/updatecheck", async (req, res) => {
+  const sxcres = await fetch(
+    `https://www.sourcexchange.net/api/products/${process.env.SXC_PRODUCT_ID}/releases`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.SXC_API_TOKEN}`,
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+    },
+  );
+  const sxcjson = await sxcres.json();
+  const cleanData = sxcjson.map(
+    ({ id, product_id, downloads_count, ...keepRemaining }) => keepRemaining,
+  );
+  res.json(cleanData);
 });
 
 app.listen(port, () => {
